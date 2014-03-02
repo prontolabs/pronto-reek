8dcd60fcf49cc31201222c83014960375e9d53e4
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2014-03-02 19:19:27 +0200

    Initial runner for Reek

---
+++ lib/pronto/reek.rb
@@ -0,0 +1,45 @@
+require 'pronto'
+require 'reek'
+
+module Pronto
+  class Reek < Runner
+    def run(patches, _)
+      return [] unless patches
+
+      patches_with_additions = patches.select { |patch| patch.additions > 0 }
+      files = patches_with_additions.map { |patch| patch.new_file_full_path.to_s }
+
+      if files.any?
+        examiner = ::Reek::Examiner.new(files)
+        messages_for(patches_with_additions, examiner.smells).compact
+      else
+        []
+      end
+    end
+
+    def messages_for(patches, errors)
+      errors.map do |error|
+        patch = patch_for_error(patches, error)
+
+        if patch
+          line = patch.added_lines.find do |added_line|
+            error.lines.find { |error_line| error_line == added_line.new_lineno }
+          end
+
+          new_message(line, error) if line
+        end
+      end
+    end
+
+    def new_message(line, error)
+      Message.new(line.patch.delta.new_file[:path], line, :warning,
+                  "#{error.message.capitalize} (#{error.subclass})")
+    end
+
+    def patch_for_error(patches, error)
+      patches.find do |patch|
+        patch.new_file_full_path.to_s == error.location['source']
+      end
+    end
+  end
+end
